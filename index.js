cc8c06f081b0d537ae7af7f84221003541a870a6
Nams <nachiyammai.al@gmail.com>
2026-07-19 19:22:42 +0530

    Added first rest api endpoint post.

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/body', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+app.post('/generateStory', (req, res) => {
+    console.log(req.body)
+  res.json({ message: 'Story generated successfully', data: req.body });
+})
