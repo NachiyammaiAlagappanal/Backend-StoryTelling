05365dd713e2fcc43e84ee9a19beb50ad38560a2
Nams <nachiyammai.al@gmail.com>
2026-07-20 09:29:16 +0530

    Added api call to gemini models via rest api.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import dotenv from 'dotenv/config';
 import express from 'express';
+import {GoogleGenAI} from "@google/genai";
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
 
 const app = express();
 
@@ -13,7 +18,15 @@ app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
-app.post('/generateStory', (req, res) => {
-    console.log(req.body)
-  res.json({ message: 'Story generated successfully', data: req.body });
-})
+
+app.post('/generateStory', async (req, res) => {
+    const {character, age} = req.body;
+    const response = await ai.models.generateContent({
+    model: "models/gemini-3.5-flash",
+    contents: `Write a short story about a ${age}-year-old ${character} and a dragon.`,
+  });
+  console.log(response);
+  res.json(response);
+});
+
+
